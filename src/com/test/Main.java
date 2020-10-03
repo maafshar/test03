1a1ee04a05791a0817bcd664db402657f930f957
Mr_Afshar <mr_afshar@isc.co.ir>
2020-10-03 15:19:39 +0330

    Initial commit

---
+++ src/com/test/Main.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.function.ToDoubleBiFunction;
+
+public class Main {
+
+    /**
+     * sample java doc
+     * @param args input sample
+     */
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("hello java...");
+        String s=new String("dd");
+        String s1=new String("سسی");
+        List<Integer> a;
+
+        for (StringTokenizer stringTokenizer = new StringTokenizer(s1); stringTokenizer.hasMoreTokens(); ) {
+            String nextToken = stringTokenizer.nextToken();
+
+        }
+
+
+    }
+
+    /**
+     * wwwww
+     * @param args - wwwwwww
+     * @return ddddddd
+     */
+    public static int main1(String[] args) {
+        return 0;
+    }
+}
